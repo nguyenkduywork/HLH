2d04ba3f0eb9cd43652ed50e0c8c0726bab907fe
nguyenkduy <nguyenkduywork@gmail.com>
2022-01-15 18:51:31 +0100

    Added a tile map system Added a script to show tile coordinates in real time

---
+++ Assets/Script/CoordinateLabeler.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    private TextMeshPro label;
+    private Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+        }
+            
+    }
+
+    void DisplayCoordinates()
+    {
+        //Vector2Int only has x and y axis
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        
+        //write coordinates in game
+        label.text = "(" + coordinates.x + ", " + coordinates.y + ")";
+    }
+}
