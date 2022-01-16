1cc131d807d02770a1bdbd02ae277c80e8bf1a1a
nguyenkduy <nguyenkduywork@gmail.com>
2022-01-16 22:32:49 +0100

    Added towers

--- Assets/Script/Waypoint.cs
+++ Assets/Script/Waypoint.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] bool isPlaceable;
+
+    private void OnMouseOver()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isPlaceable)
+            {
+                Debug.Log(transform.name);
+            }
+            else
+            {
+                Debug.Log("This tile is not placeable!");
+            }
+        }
     }
 }
